74e368effca5c07b5d6fabe3cd8055eb73fa9bb7
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:29:00 +0100

    Creacion del getters de localizacion del la clase pokemon legendario

--- src/Pokemons/PokemonLegendario.java
+++ src/Pokemons/PokemonLegendario.java
@@ -4,18 +4,22 @@ public class PokemonLegendario extends  Pokemon {
 
     private String localizacion;
 
-
     public PokemonLegendario(String nombre, String tipo, double altura, double peso, String descripcion, String localizacion) {
         super(nombre, tipo, altura, peso, descripcion);
         this.localizacion = localizacion;
     }
 
 
-
     public PokemonLegendario(String nombre, String tipo, String localizacion) {
         super(nombre, tipo);
         this.localizacion = localizacion;
     }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+
 
 
 
