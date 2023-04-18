13c81eeea557159154d12a638762ecf23a026b6b
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:50:43 +0100

    Feature:Creacion de la instancia del objecto y poner el scanner para el menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import Pokemons.ListaPokemon;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        ListaPokemon avistados = new ListaPokemon();
+        ListaPokemon capturados = new ListaPokemon();
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String opcion = "";
+
+
 
     }
 }
