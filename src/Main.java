d4323c23d5f4af12110bf6a00bd2ef6f78e1b981
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:52:48 +0100

    Feature:Creacion del menu usando un while

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String opcion = "";
 
+        while(!opcion.equals("f")) {
+            System.out.println("Seleccione una opción:");
+            System.out.println("a - Añadir un pokemon avistado");
+            System.out.println("b - Añadir avistamiento legendario");
+            System.out.println("c - Pasar a capturados");
+            System.out.println("d - Mostrar lista de avistados");
+            System.out.println("e - Mostrar lista de capturados");
+            System.out.println("f - Cerrar la pokedex");
+            opcion = scanner.nextLine();
 
 
-    }
+
+        }
 }
