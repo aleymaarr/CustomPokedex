743eda1bf58e38493f37fe2bf52629ec2f556b68
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:27:17 +0100

    Fix:Arreglos de constructores duplicados en la clase pokemon legendario

--- src/Pokemons/PokemonLegendario.java
+++ src/Pokemons/PokemonLegendario.java
@@ -4,20 +4,13 @@ public class PokemonLegendario extends  Pokemon {
 
     private String localizacion;
 
-    public PokemonLegendario(String nombre, String tipo, double altura, double peso, String descripcion, String localizacion) {
-        super(nombre, tipo, altura, peso, descripcion);
-        this.localizacion = localizacion;
-    }
 
     public PokemonLegendario(String nombre, String tipo, double altura, double peso, String descripcion, String localizacion) {
         super(nombre, tipo, altura, peso, descripcion);
         this.localizacion = localizacion;
     }
 
-    public PokemonLegendario(String nombre, String tipo, String localizacion) {
-        super(nombre, tipo);
-        this.localizacion = localizacion;
-    }
+
 
     public PokemonLegendario(String nombre, String tipo, String localizacion) {
         super(nombre, tipo);
