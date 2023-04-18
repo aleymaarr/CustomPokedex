e90ed362ac2e557ee84392a1775c2599937a2b63
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:44:30 +0100

    Fix:Arreglos en la posicion del array y cambiado el size por el tamano

--- src/Pokemons/ListaPokemon.java
+++ src/Pokemons/ListaPokemon.java
@@ -2,37 +2,37 @@ package Pokemons;
 
 public class ListaPokemon {
     private Pokemon[] pokemons;
-    private int size;
+    private int tamano;
 
     public ListaPokemon() {
-        pokemons = new Pokemon[];
-        size = 0;
+        pokemons = new Pokemon[1];
+        tamano = 0;
     }
 
     public void agregarPokemon(Pokemon pokemon) {
-        if (size == pokemons.length) {
+        if (tamano == pokemons.length) {
             Pokemon[] newPokemons = new Pokemon[pokemons.length * 2];
             for (int i = 0; i < pokemons.length; i++) {
                 newPokemons[i] = pokemons[i];
             }
             pokemons = newPokemons;
         }
-        pokemons[size] = pokemon;
-        size++;
+        pokemons[tamano] = pokemon;
+        tamano++;
         System.out.println(pokemon.getNombre() + " ha sido agregado a la lista.");
     }
 
 
     public void eliminarPokemon(String nombre) {
         boolean encontrado = false;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < tamano; i++) {
             if (pokemons[i].getNombre().equals(nombre)) {
                 encontrado = true;
-                for (int j = i; j < size - 1; j++) {
+                for (int j = i; j < tamano - 1; j++) {
                     pokemons[j] = pokemons[j + 1];
                 }
-                pokemons[size - 1] = null;
-                size--;
+                pokemons[tamano - 1] = null;
+                tamano--;
                 System.out.println(nombre + " ha sido eliminado de la lista.");
                 break;
             }
@@ -44,17 +44,17 @@ public class ListaPokemon {
 
     public void mostrarPokemons() {
         System.out.println("Lista de Pokemons:");
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < tamano; i++) {
             System.out.println((i+1) + ". " + pokemons[i].toString());
         }
     }
 
     public Pokemon obtenerPokemon(int posicion) {
-        if (posicion >= 0 && posicion < size) {
+        if (posicion >= 0 && posicion < tamano) {
             return pokemons[posicion];
         } else {
             return null;
         }
     }
 }
-}
+
