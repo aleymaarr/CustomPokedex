6e53e9815f7ce4d76bfe4817906e3f6fb6c3bd4f
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:10:07 +0100

    Feature:Creacion de los contructores para la clase pokemon

--- src/Pokemons/Pokemon.java
+++ src/Pokemons/Pokemon.java
@@ -6,4 +6,24 @@ public class Pokemon {
     private double altura;
     private double peso;
     private String descripcion;
+
+    public Pokemon(String nombre, String tipo, double altura, double peso, String descripcion) {
+
+    }
+
+    public Pokemon(String nombre, String tipo, double altura, double peso, String descripcion) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.descripcion = descripcion;
+    }
+
+    public Pokemon(String nombre, String tipo) {
+
+    }
+    public Pokemon(String nombre, String tipo) {
+        this(nombre, tipo, 0, 0, "");
+    }
+
 }
