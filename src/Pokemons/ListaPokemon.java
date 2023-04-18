8bd15b5b675b91dfab59ebd53c6bc409c544953a
aleymarr <aleyba2002@gmail.com>
2023-04-18 10:40:54 +0100

    Feature:Creacion del contructor de la clase pokemon lista

--- src/Pokemons/ListaPokemon.java
+++ src/Pokemons/ListaPokemon.java
@@ -3,4 +3,11 @@ package Pokemons;
 public class ListaPokemon {
     private Pokemon[] pokemons;
     private int size;
+
+    public ListaPokemon() {
+        pokemons = new Pokemon[];
+        size = 0;
+    }
+
+
 }
